58a08d8aea8f4234c68899c4ca4fd6c4bce9213d
Katarzyna <34576130+KatarzynaSkotnicka@users.noreply.github.com>
2018-02-08 09:52:33 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var url = 'http://api.icndb.com/jokes/random',
+var url = 'https://api.icndb.com/jokes/random',
   paragraph = document.getElementById('joke'),
 
   button = document.getElementById('get-joke');
